ed0a7dca71625c44218a8b59b45ea0a99300d92f
Cream2015 <343224963@qq.com>
2016-03-24 22:48:40 +0800

    添加系统平台设置

--- Simple/src/Simple/Controllers/AdminController.cs
+++ Simple/src/Simple/Controllers/AdminController.cs
@@ -3,17 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.AspNet.Authorization;
+using Simple.Models;
 
 namespace Simple.Controllers
 {
-    public class AdminController : Controller
+    [Authorize(Roles = "系统管理员")]
+    public class AdminController : BaseController
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult JoinShopType()
+        {
+            var type = DB.FindTypes.OrderBy(x => x.Id).ToList();
+            return View(type);
+        }
+        [HttpGet]
+        public IActionResult CreateJoinShopType()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateJoinShopType(FindType findtype)
+        {
+            DB.FindTypes.Add(findtype);
+            DB.SaveChanges();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult EditJoinShopType(int id)
+        {
+            var ret = DB.FindTypes.Where(x => x.Id == id).SingleOrDefault();
+            return View(ret);
+        }
+        [HttpPost]
+        public IActionResult EditJoinShopType(int id,FindType findtype)
+        {
+            var old= DB.FindTypes.Where(x => x.Id == id).SingleOrDefault();
+            if (old == null)
+            {
+                return Content("该进入店铺方式不存在");
+            }
+            else
+            {
+                old.Type = findtype.Type;
+                DB.SaveChanges();
+                return RedirectToAction("JoinShopType", "Admin");
+            }
         }
     }
 }
