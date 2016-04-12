76cd5961be44aa112be014a5baa5bb45f531002d
Cream2015 <343224963@qq.com>
2016-04-12 11:40:48 +0800

    新增note字段和price字段

--- Simple/src/Simple/Models/NextOrToday.cs
+++ Simple/src/Simple/Models/NextOrToday.cs
@@ -9,6 +9,7 @@ namespace Simple.Models
     {
         public int Id { get; set; }
         public string Type { get; set; }//隔天下单，当天下单属性
-        public string Price { get; set; }//对应价格
+        public double Price { get; set; }//对应价格
+        public string Note { get; set; }//说明
     }
 }
