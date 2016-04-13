c862d092a623db4680e63ff1ca2493e07b748770
Cream2015 <343224963@qq.com>
2016-04-13 10:12:04 +0800

    添加关于我们字段

---
+++ Simple/src/Simple/Models/AboutUs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple.Models
+{
+    public class AboutUs
+    {
+        //关于我们
+        public int Id { get; set; }
+        public int QQ { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
