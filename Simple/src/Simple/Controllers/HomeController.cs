ffbefa70eb14e1b0ea8ccf88caa9f752c79f9e0e
Cream2015 <343224963@qq.com>
2016-04-16 22:24:11 +0800

    home fixed  display orders

--- Simple/src/Simple/Controllers/HomeController.cs
+++ Simple/src/Simple/Controllers/HomeController.cs
@@ -15,71 +15,244 @@ namespace Simple.Controllers
         {
             if (User.IsInRole("系统管理员"))
             {
-                //管理员Index页面中显示订单数
-                var oa = DB.PreOrders.Count();//总单数
+                //管理员Index页面中显示Ebay订单数
+                var oa = DB.PreOrders
+                    .Count();//总单数
                 ViewBag.PreOrderAll = oa;
-                var ob = DB.PreOrders.Where(x => x.Draw == Draw.待审核).Count();//待审核
+
+                var ob = DB.PreOrders
+                    .Where(x => x.Draw == Draw.待审核)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Where(x => x.IsPayfor == IsPayFor.未支付)
+                    .Where(x => x.State == State.未锁定)
+                    .Count();//待审核
                 ViewBag.PreOrderWaitDraw = ob;
-                var oc = DB.PreOrders.Where(x => x.Draw == Draw.通过).Where(x => x.IsFinish == IsFinish.未完成).Count();//审核待完成
+
+                var oc = DB.PreOrders
+                    .Where(x => x.Draw == Draw.通过)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Where(x => x.IsPayfor == IsPayFor.未支付)
+                    .Where(x => x.State == State.未锁定)
+                    .Count();//待支付
                 ViewBag.PreOrderDraw = oc;
-                var od = DB.PreOrders.Where(x => x.IsFinish == IsFinish.已完成).Count();//已完成订单
+
+                var od = DB.PreOrders
+                    .Where(x => x.Draw == Draw.通过)
+                    .Where(x => x.IsPayfor == IsPayFor.已支付)
+                    .Where(x => x.State == State.锁定)
+                    .Where(x => x.IsFinish == IsFinish.已完成)
+                    .Count();//已完成订单
                 ViewBag.PreOrderFinish = od;
+
+                var of = DB.PreOrders
+                    .Where(x => x.Draw == Draw.未通过)
+                    .Where(x => x.IsPayfor == IsPayFor.未支付)
+                    .Where(x => x.State == State.未锁定)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Count();//审核未通过订单
+                ViewBag.PreOrderNotPass = of;
+
+                var og = DB.PreOrders
+                    .Where(x => x.Draw == Draw.通过)
+                    .Where(x => x.IsPayfor == IsPayFor.已支付)
+                    .Where(x => x.State == State.锁定)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Count();//审核进行中订单
+                ViewBag.PreOrderIng = og;
+
                 var oda = (double)od / oa;
                 ViewBag.oda = Math.Round(oda, 2) * 100;//已完成
+
                 var oba = (double)ob / oa;
                 ViewBag.oba = Math.Round(oba, 2) * 100;//待审核
-                var oca = (double)oc / oa;
-                ViewBag.oca = Math.Round(oca, 2) * 100;//已审核待完成
-                //管理员Index页面helpful总单数显示
-                var a = DB.HelpfulPreOrders.Count();//总单数
-                ViewBag.HelpfulAll = a;
-                var b = DB.HelpfulPreOrders.Where(x => x.Draw == Draw.待审核).Count();//待审核
-                ViewBag.HelpfulWaitDraw = b;
-                var c = DB.HelpfulPreOrders.Where(x => x.Draw == Draw.通过).Where(x=>x.IsFinish==IsFinish.未完成).Count();//审核待完成
-                ViewBag.HelpfulDraw = c;
-                var d = DB.HelpfulPreOrders.Where(x => x.IsFinish == IsFinish.已完成).Count();//已完成订单
-                ViewBag.HelpfulFinish = d;
-                var da = (double)d / a;
-                ViewBag.da = Math.Round(da, 2) * 100;//已完成
-                var ba = (double)b / a;
-                ViewBag.ba = Math.Round(ba, 2) * 100;//待审核
-                var ca = (double)c / a;
-                ViewBag.ca = Math.Round(ca, 2) * 100;//已审核待完成
-            }
-            else
-            {
-                //用户Index页面中显示订单数
-                var oa = DB.PreOrders.Where(x=>x.UserId==UserCurrent.Id).Count();//总单数
-                ViewBag.PreOrderAll = oa;
-                var ob = DB.PreOrders.Where(x=>x.UserId==UserCurrent.Id).Where(x => x.Draw == Draw.待审核).Count();//待审核
-                ViewBag.PreOrderWaitDraw = ob;
-                var oc = DB.PreOrders.Where(x => x.UserId == UserCurrent.Id).Where(x => x.Draw == Draw.通过).Where(x=>x.IsFinish==IsFinish.未完成).Count();//审核待完成
-                ViewBag.PreOrderDraw = oc;
-                var od = DB.PreOrders.Where(x => x.UserId == UserCurrent.Id).Where(x => x.IsFinish == IsFinish.已完成).Count();//已完成订单
-                ViewBag.PreOrderFinish = od;
-                var oda = (double)od / oa;
-                ViewBag.oda = Math.Round(oda, 2) * 100;//已完成
-                var oba = (double)ob / oa;
-                ViewBag.oba = Math.Round(oba, 2) * 100;//待审核
+
                 var oca = (double)oc / oa;
                 ViewBag.oca = Math.Round(oca, 2) * 100;//已审核待完成
 
+                var ofa = (double)of / oa;
+                ViewBag.ofa = Math.Round(ofa, 2) * 100;//审核未通过订单
+
+                var oga = (double)og / oa;
+                ViewBag.oga = Math.Round(oga, 2) * 100;//进行中订单
+
+
                 //用户Index页面Helpful订单展示
-                
-                var a = DB.HelpfulPreOrders.Where(x => x.UserId == UserCurrent.Id).Count();
+
+                var a = DB.HelpfulPreOrders
+                    .Count();
                 ViewBag.HelpfulAll = a;
-                var b = DB.HelpfulPreOrders.Where(x => x.UserId == UserCurrent.Id).Where(x => x.Draw == Draw.待审核).Count();//待审核
+
+                var b = DB.HelpfulPreOrders
+                    .Where(x => x.Draw == Draw.待审核)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Where(x => x.IsPayFor == IsPayFor.未支付)
+                    .Where(x => x.State == State.未锁定)
+                    .Count();//待审核
                 ViewBag.HelpfulWaitDraw = b;
-                var c = DB.HelpfulPreOrders.Where(x => x.UserId == UserCurrent.Id).Where(x => x.Draw == Draw.通过).Where(x=>x.IsFinish==IsFinish.未完成).Count();//审核待完成
+
+                var c = DB.HelpfulPreOrders
+                    .Where(x => x.Draw == Draw.通过)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Where(x => x.IsPayFor == IsPayFor.未支付)
+                    .Where(x => x.State == State.未锁定)
+                    .Count();//待支付订单
                 ViewBag.HelpfulDraw = c;
-                var d = DB.HelpfulPreOrders.Where(x => x.UserId == UserCurrent.Id).Where(x => x.IsFinish == IsFinish.已完成).Count();//已完成订单
+
+                var d = DB.HelpfulPreOrders
+                    .Where(x => x.Draw == Draw.通过)
+                    .Where(x => x.IsFinish == IsFinish.已完成)
+                    .Where(x => x.IsPayFor == IsPayFor.已支付)
+                    .Where(x => x.State == State.锁定)
+                    .Count();//已完成订单
                 ViewBag.HelpfulFinish = d;
+
+                var f = DB.HelpfulPreOrders
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Where(x => x.IsPayFor == IsPayFor.未支付)
+                    .Where(x => x.State == State.未锁定)
+                    .Where(x => x.Draw == Draw.未通过)
+                    .Count();//审核未通过订单
+                ViewBag.HelpfulNotPass = f;
+
+                var g = DB.HelpfulPreOrders
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Where(x => x.IsPayFor == IsPayFor.已支付)
+                    .Where(x => x.Draw == Draw.通过)
+                    .Where(x => x.State == State.锁定)
+                    .Count();//进行中订单
+                ViewBag.HelpfulOrderIng = g;
+
+                var da = (double)d / a;
+                ViewBag.da = Math.Round(da, 2) * 100;//已完成
+
+                var ba = (double)b / a;
+                ViewBag.ba = Math.Round(ba, 2) * 100;//待审核
+
+                var ca = (double)c / a;
+                ViewBag.ca = Math.Round(ca, 2) * 100;//待支付
+
+                var fa = (double)f / a;
+                ViewBag.fa = Math.Round(fa, 2) * 100;//审核未通过
+
+                var ga = (double)g / a;
+                ViewBag.ga = Math.Round(ga, 2) * 100;//进行中
+            }
+            else
+            {
+                //用户Index页面中显示Ebay订单数
+                var oa = DB.PreOrders
+                    .Where(x=>x.UserId==UserCurrent.Id)
+                    .Count();//总单数
+                ViewBag.PreOrderAll = oa;
+                var ob = DB.PreOrders
+                    .Where(x=>x.UserId==UserCurrent.Id)
+                    .Where(x => x.Draw == Draw.待审核)
+                    .Where(x=>x.IsFinish==IsFinish.未完成)
+                    .Where(x=>x.IsPayfor==IsPayFor.未支付)
+                    .Where(x=>x.State==State.未锁定)
+                    .Count();//待审核
+                ViewBag.PreOrderWaitDraw = ob;
+                var oc = DB.PreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Where(x => x.Draw == Draw.通过)
+                    .Where(x=>x.IsFinish==IsFinish.未完成)
+                    .Where(x=>x.IsPayfor==IsPayFor.未支付)
+                    .Where(x=>x.State==State.未锁定)
+                    .Count();//待支付
+                ViewBag.PreOrderWaitPay = oc;
+                var od = DB.PreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Where(x=>x.Draw==Draw.通过)
+                    .Where(x=>x.IsPayfor==IsPayFor.已支付)
+                    .Where(x=>x.State==State.锁定)
+                    .Where(x => x.IsFinish == IsFinish.已完成)
+                    .Count();//已完成订单
+                ViewBag.PreOrderFinish = od;
+                var of = DB.PreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Where(x => x.Draw == Draw.未通过)
+                    .Where(x => x.IsPayfor == IsPayFor.未支付)
+                    .Where(x => x.State == State.未锁定)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Count();//审核未通过订单
+                ViewBag.PreOrderNotPass = of;
+                var og = DB.PreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Where(x => x.Draw == Draw.通过)
+                    .Where(x => x.IsPayfor == IsPayFor.已支付)
+                    .Where(x => x.State == State.锁定)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Count();//审核进行中订单
+                ViewBag.PreOrderIng = og;
+                var oda = (double)od / oa;
+                ViewBag.oda = Math.Round(oda, 2) * 100;//已完成
+                var oba = (double)ob / oa;
+                ViewBag.oba = Math.Round(oba, 2) * 100;//待审核
+                var oca = (double)oc / oa;
+                ViewBag.oca = Math.Round(oca, 2) * 100;//已审核待完成
+                var ofa = (double)of / oa;
+                ViewBag.ofa = Math.Round(ofa, 2) * 100;//审核未通过订单
+                var oga = (double)og / oa;
+                ViewBag.oga = Math.Round(oga, 2) * 100;//进行中订单
+
+
+                //用户Index页面Helpful订单展示
+
+                var a = DB.HelpfulPreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Count();
+                ViewBag.HelpfulAll = a;
+                var b = DB.HelpfulPreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Where(x => x.Draw == Draw.待审核)
+                    .Where(x=>x.IsFinish==IsFinish.未完成)
+                    .Where(x=>x.IsPayFor==IsPayFor.未支付)
+                    .Where(x=>x.State==State.未锁定)
+                    .Count();//待审核
+                ViewBag.HelpfulWaitDraw = b;
+                var c = DB.HelpfulPreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Where(x => x.Draw == Draw.通过)
+                    .Where(x=>x.IsFinish==IsFinish.未完成)
+                    .Where(x=>x.IsPayFor==IsPayFor.未支付)
+                    .Where(x=>x.State==State.未锁定)
+                    .Count();//待支付订单
+                ViewBag.HelpfulDraw = c;
+                var d = DB.HelpfulPreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Where(x=>x.Draw==Draw.通过)
+                    .Where(x => x.IsFinish == IsFinish.已完成)
+                    .Where(x=>x.IsPayFor==IsPayFor.已支付)
+                    .Where(x=>x.State==State.锁定)
+                    .Count();//已完成订单
+                ViewBag.HelpfulFinish = d;
+                var f = DB.HelpfulPreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Where(x=>x.IsPayFor==IsPayFor.未支付)
+                    .Where(x=>x.State==State.未锁定)
+                    .Where(x=>x.Draw==Draw.未通过)
+                    .Count();//审核未通过订单
+                ViewBag.HelpfulNotPass = f;
+                var g = DB.HelpfulPreOrders
+                    .Where(x => x.UserId == UserCurrent.Id)
+                    .Where(x => x.IsFinish == IsFinish.未完成)
+                    .Where(x=>x.IsPayFor==IsPayFor.已支付)
+                    .Where(x=>x.Draw==Draw.通过)
+                    .Where(x=>x.State==State.锁定)
+                    .Count();//进行中订单
+                ViewBag.HelpfulOrderIng = g;
                 var da = (double)d / a;
                 ViewBag.da = Math.Round(da, 2) * 100;//已完成
                 var ba = (double)b / a;
                 ViewBag.ba = Math.Round(ba, 2) * 100;//待审核
                 var ca = (double)c / a;
                 ViewBag.ca = Math.Round(ca, 2) * 100;//已审核待完成
+                var fa = (double)f / a;
+                ViewBag.fa = Math.Round(fa, 2) * 100;//待审核
+                var ga = (double)g / a;
+                ViewBag.ga = Math.Round(ga, 2) * 100;//待审核
+
             }
             return View();
         }
@@ -100,16 +273,26 @@ namespace Simple.Controllers
                 .Where(x => x.PreOrderNumber == key || x.ShopName.Contains(key) || x.TrueOrderNumber == key)
                 .OrderBy(x => x.PostTime)
                 .ToList();
-            return View(orders);
+            var helpful = DB.HelpfulPreOrders
+                .Where(x => x.UserId == UserCurrent.Id)
+                .Where(x => x.OrderNumber == key)
+                .SingleOrDefault();
+                ViewBag.HelpfulOrder = helpful;
+                return View(orders);
+            
         }
         [HttpPost]
         public IActionResult UserSearch(string key)
         {
             var orders = DB.PreOrders
-               .Where(x=>x.UserId==UserCurrent.Id)
-               .Where(x => x.PreOrderNumber == key||x.ShopName.Contains(key)||x.TrueOrderNumber==key)
-               .ToList();
-            if (orders.Count() == 0)
+               .Where(x => x.UserId == UserCurrent.Id)
+               .Where(x => x.PreOrderNumber == key || x.ShopName.Contains(key) || x.TrueOrderNumber == key)
+               .Count();
+            var helpful = DB.HelpfulPreOrders
+                .Where(x => x.UserId == UserCurrent.Id)
+                .Where(x => x.OrderNumber == key)
+                .SingleOrDefault();
+            if (orders== 0&&helpful==null)
             {
                 return Content("error");
             }
