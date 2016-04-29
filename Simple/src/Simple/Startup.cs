beff1b3118008851c5f44d463f30c11203fd78ee
IceMao <18845294217@163.com>
2016-04-29 13:46:54 +0800

    系统管理正则添加

--- Simple/src/Simple/Startup.cs
+++ Simple/src/Simple/Startup.cs
@@ -24,12 +24,12 @@ namespace Simple
         {
             var appEnv = services.BuildServiceProvider().GetRequiredService<IApplicationEnvironment>();
 
-            //services.AddEntityFramework()
-            //    .AddSqlServer()
-            //    .AddDbContext<SimpleContext>(x => x.UseSqlServer("Server=localhost;uid=sa;password=Cream2015!@#;database=simple;"));
             services.AddEntityFramework()
-                .AddSqlite()
-                .AddDbContext<SimpleContext>(x => x.UseSqlite("Data source=" + appEnv.ApplicationBasePath + "/Database/simple.db"));
+                .AddSqlServer()
+                .AddDbContext<SimpleContext>(x => x.UseSqlServer("Server=localhost;uid=sa;password=Cream2015!@#;database=simple;"));
+            //services.AddEntityFramework()
+            //    .AddSqlite()
+            //    .AddDbContext<SimpleContext>(x => x.UseSqlite("Data source=" + appEnv.ApplicationBasePath + "/Database/simple.db"));
             services.AddIdentity<User, IdentityRole>(x=> {
                 x.Password.RequireDigit = false;
                 x.Password.RequiredLength = 0;
